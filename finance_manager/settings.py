bc062e7422bc92bf989a6b18536ff79be775b9b7
David <128511591+chaos-hunter@users.noreply.github.com>
2025-07-22 11:32:05 -0400

    Update settings.py

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 
 ALLOWED_HOSTS = ['DavidEntonu.pythonanywhere.com']
-
+CSRF_TRUSTED_ORIGINS = ['https://davidentonu.pythonanywhere.com']
 
 
 # Application definition
@@ -144,17 +144,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://*.ngrok-free.app',
-]
+
 PASSWORD_RESET_TIMEOUT = 15 * 60
